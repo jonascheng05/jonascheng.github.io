e54fc49cf1e3dbeeb874f58b0fd320af76694422
jonascheng05 <156994886+jonascheng05@users.noreply.github.com>
2024-01-18 00:42:15 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor=> {
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Get the button
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    };
+
+    // When the user clicks on the button, scroll to the top of the document
+    scrollToTopBtn.addEventListener("click", function () {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    });
+
+});
